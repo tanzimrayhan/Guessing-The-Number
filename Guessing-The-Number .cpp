7ae83d212236584208d22fbc39ad91249af64b84
Tanzimrayhan <tanzim_rayhan@yahoo.com>
2017-03-21 13:43:34 -0400

    This is the cpp file

---
+++ Guessing-The-Number .cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main() {
+	unsigned seed = time(0);
+	srand(seed);
+
+	int a, b, count=0;
+	a = rand()%100 ;
+	cout << "The computer has a randomized number stored in the memory \nGuess the number " << endl;
+	cin >> b;
+
+	while (a > b||a<b){
+		if (b>a) {
+			cout << "Too high, try again " << endl;
+			count++;
+	}
+		else if (a>b) {
+			cout << "Too low, try again " << endl;
+			count++;
+		}
+		cin >> b;
+
+	};
+	if (a=b) {
+		cout << "Your guess is right! Congratz. " << endl;
+		count++;
+	}
+	cout << "Number of guess[es] = " << count << endl;
+
+	cin.get();
+	cin.get();
+	return 0;
+	
+}
